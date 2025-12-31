acf2ac9a1febd9efb1bd8738fd31684b1c5d8305
키위 <slalom0914@gmail.com>
2025-12-31 11:16:05 +0900

    라우터 적용완료

--- nameCard-step1/src/App.jsx
+++ nameCard-step1/src/App.jsx
@@ -1,15 +1,26 @@
-import './App.css';
-//리액트에서는 태그를 만들어 쓴다.
-//주의: 콤포넌트의 이름을 반드시 대문자로 한다.
-//화살표 함수로 변경한 부분
-//() => ()
-//() => {}
+import { Routes, Route } from "react-router";
+import styled from "styled-components";
+
+const AppDiv = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e8e7e6;
+`  
 const App = () => {
+
   //선언부
   return (
-    <div className="App">
-      Hello React!!!
-    </div>
+    <>
+      <AppDiv>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/maker" element={<Maker />} />
+        </Routes>
+      </AppDiv>
+    </>
   );
 }
 
--- nameCard-step1/src/index.js
+++ nameCard-step1/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router';
+// public>index.html div 위치 파악
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </>
 );
 
