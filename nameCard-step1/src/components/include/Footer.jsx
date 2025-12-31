df81d2db97845cb5d16943f565ea39a98931582d
키위 <slalom0914@gmail.com>
2025-12-31 13:04:15 +0900

    로그인페이지완성

---
+++ nameCard-step1/src/components/include/Footer.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const FooterDiv = styled.div`
+    width: 100%;
+    background-color: #DBA39A;
+    text-align: center;    
+    border-radius: 0 0 30px 30px;
+`
+const TitleP = styled.p`
+    color: white;
+`
+const Footer = () => {
+  return (
+    <>
+      <FooterDiv>
+        <TitleP>No pain, No gain</TitleP>
+      </FooterDiv>
+    </>
+  )
+}
+
+export default Footer
