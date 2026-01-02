64fe0d34864d6593664e46c17a8db1a68196e221
키위 <slalom0914@gmail.com>
2026-01-02 13:06:01 +0900

    4교시-Card 가디자인

--- nameCard-step1/src/components/pages/Preview.jsx
+++ nameCard-step1/src/components/pages/Preview.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import Card from "./Card"
 
 const PreviewDiv = styled.div`
     flex-basis: 50%;
@@ -24,8 +25,8 @@ const Preview = ({cards}) => {
       <TitleH1>Card Preview</TitleH1>
       <CardsUl>
       {
-        Object.keys(cards).map(card => (
-          <li>card</li>
+        Object.keys(cards).map(key => (
+          <Card key={key} card={cards[key]} />
         ))
       }
       </CardsUl>
