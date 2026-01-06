52fe348c6f22d5804a4f009f44382a60627ebcef
키위 <slalom0914@gmail.com>
2026-01-06 11:04:41 +0900

    이미지 파일 업로드 처리

--- nameCard-step1/src/components/common/ImageFileInput.jsx
+++ nameCard-step1/src/components/common/ImageFileInput.jsx
@@ -12,19 +12,32 @@ const ContainerDiv = styled.div`
 const HiddenInput = styled.input`
   display: none;
 `
-const ImageFileInput = ({name}) => {
+//사용자 컴포넌트는 함수이다. - 일급객체, 일급함수, 고차함수
+//화면이 다시 그려지면 함수도 새로 만들어진다.- 비효율적임(부분갱신이나 부분처리 가능함)
+const ImageFileInput = ({name, imageUploader}) => {
   // input type file은 숨기고 버튼을 눌렀을 때 file컴포넌트 동작시키기
   // file 컴포넌트가 loading과 같은 상태가 바뀔 때마다 다시 생성된다.???
   // 상태가 바뀌더라도 원래값을 유지해주는 훅이 있다. - useRef
   const inputRef = useRef() //앞에 use로 시작되는 함수는 모두 Hook임
   // file명을 선택하고 업로드 중일 때 상태 관리
+  //const a = 10 //a에 값이 변하더라도 새로 그려지지 않음
+  //일반변수 a는 값이 아무리 변경되더라도 다시 그려지지 않는다.
+  //함수가 새로 생성되지 않는다 | 새로 생성됨. -> useEffect에 의존성배열 관련
+  //useEffect내부에 실행문이 실행이 된다 | 실행이 되지 않는다.
   const [loading, setLoading] = useState(false)
-  const handleChange = () => {
-
-  }
+  const handleChange = async (event) => {
+    //로딩 애니메이션 효과 발동 -> 상태값을 false -> true변경해줌
+    setLoading(true)
+    console.log(event.target.files[0])
+    const uploaded = await imageUploader.upload(event.target.files[0])
+    console.log(uploaded) 
+    
+  }//end of file
   // button의 디폴트 type은 submit이다.
+  // submit이슈
   const onButtonClick = (event) => {
-    event.preventDefault()
+    event.preventDefault() //의도하지 않은 이벤트가 전이되지 않도록
+    //폼전송에서 사용하면 사용자가 입력한 값을 잃어버린다.
     // No file이 클릭되었을 때 버튼 클릭하기
     inputRef.current.click()
   }
--- nameCard-step1/src/index.js
+++ nameCard-step1/src/index.js
@@ -7,6 +7,10 @@ import ImageFileInput from './components/common/ImageFileInput';
 const imageUploader = new ImageUploader() 
 // 왜 CardEditor에서 사용할 컴포넌트를 index.js에 선언하였나?
 // props는 상위 컴포넌트에서 하위 컴포넌트로 만 전달 가능함.
+// () => ()
+// () => {return} 
+// ... spread operation
+// 이거가 없다면 새로고침이 된다.누적이 안됨 -> 덮어쓰기와 같다. 
 const FileInput = props => (
   <ImageFileInput {...props} imageUploader={imageUploader} />
 )
